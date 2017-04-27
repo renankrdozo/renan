bc69e4d242090ee754c992e62d72bcd785dd7778
Alunos <Alunos@PC-Lab3>
2017-04-26 22:08:56 -0300

    adicionando pom.xml

--- bolicho.modelo/src/br/edu/iffarroupilha/bolicho/modelo/dao/HibernateDAO.java
+++ bolicho.modelo/src/br/edu/iffarroupilha/bolicho/modelo/dao/HibernateDAO.java
@@ -1,6 +1,7 @@
 package br.edu.iffarroupilha.bolicho.modelo.dao;
 
 import org.hibernate.Session;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -8,16 +9,17 @@ import br.edu.iffarroupilha.bolicho.modelo.Cliente;
 
 public class HibernateDAO {
 	private static SessionFactory fabrica;
-	static{
-		//depois adicionar as demais entidades
-		//apos ter feito o mapeamento
-		Configuration ctg= new Configuration();
+	static {
+		// depois adicionar as demais entidades
+		// apos ter feito o mapeamento
+		Configuration ctg = new Configuration();
 		ctg.addAnnotatedClass(Cliente.class);
 		ctg.configure();
 		// forma mains facio de criar fabrica
 		fabrica = ctg.buildSessionFactory();
 	}
-	public static Session getSessao(){
+
+	public static Session getSessao() {
 		return fabrica.openSession();
 	}
 

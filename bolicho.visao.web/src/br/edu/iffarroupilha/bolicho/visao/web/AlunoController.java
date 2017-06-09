0ca8f60c09451ad636c57f094c9c330bbce52341
Alunos <Alunos@PC-Lab3>
2017-06-08 22:01:43 -0300

    MOVO ALUNO

--- bolicho.visao.web/src/br/edu/iffarroupilha/bolicho/visao/web/AlunoController.java
+++ bolicho.visao.web/src/br/edu/iffarroupilha/bolicho/visao/web/AlunoController.java
@@ -2,16 +2,16 @@ package br.edu.iffarroupilha.bolicho.visao.web;
 
 import java.util.List;
 
+import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
-import br.edu.iffarroupilha.bolicho.controle.ClienteControle;
+import br.edu.iffarroupilha.bolicho.controle.AlunoControle;
 import br.edu.iffarroupilha.bolicho.modelo.Aluno;
-import br.edu.iffarroupilha.bolicho.modelo.Cliente;
-
-public class AlunoControler {
-	private AlunoControler controle;
+@Resource
+public class AlunoController {
+	private AlunoControle controle;
 	private Result result;
 
-	public AlunoControler(AlunoControler controle,
+	public AlunoController(AlunoControle controle,
 			                           Result result) {
 		this.controle = controle;
 		this.result = result;
@@ -22,10 +22,7 @@ public class AlunoControler {
 		result.include("Aluno", itens);
 	}
 	
-	private List buscarTodos(Class<Aluno> class1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 
 	// carrega a visao para adicionar um item 
 	public void cadastrar(){
